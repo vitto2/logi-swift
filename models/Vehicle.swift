9c267b9d93e1f7ad7018dc572e969b04b6a4dd04
Vitor Silva <vitor2071@gmail.com>
2025-12-17 05:12:33 -0300

    feat: criar model Vehicle

---
+++ models/Vehicle.swift
@@ -0,0 +1,25 @@
+class Vehicle {
+
+  private let id: String 
+  private let loadCapacity: Double
+  private let inRoute: Bool
+  
+  init(loadCapacity: Double, inRoute: Bool) {
+      let idGenerate = Int.random(in: 1000...2000)
+
+      self.loadCapacity = loadCapacity
+      self.inRoute = inRoute
+      self.id = String(idGenerate)
+  }
+  
+
+  func prepareForTravel() { 
+
+    guard inRoute else { 
+      print("O veículo ainda não está em rota. ")
+      return
+    }
+
+    print("O veículo está em rota.")
+  }
+}
